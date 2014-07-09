8871991f1c957185a1ef981a907de6952b704168
yu91kuzmin <yu91kuzmin@gmail.com>
2014-07-09 11:55:30 +0400

    first
    protobuf

---
+++ proto_new/proto_new/proto_new.cpp
@@ -0,0 +1,22 @@
+// proto.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "DocumentId.pb.h"
+#include "stdafx.h"
+#include <iostream>
+
+#pragma comment(lib,"libprotobuf.lib")
+#pragma comment(lib,"libprotobuf-lite.lib")
+#pragma comment(lib,"libprotoc.lib")
+
+using namespace std;
+using namespace Diadoc::Api::Proto;
+
+
+int _tmain()
+{
+	DocumentId doc_id;
+	//getchar();
+	return 0;
+}
+
